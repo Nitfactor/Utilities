80a2c4333b2eb874756854d5573abdd0165e170f
Akshat Singh <nitfactor@gmail.com>
2026-02-05 23:48:48 +0530

    better day4 solution

--- day_4_2.py
+++ day_4_2.py
@@ -2,15 +2,26 @@
 #Store them in a list.
 #Print the largest number.
 
-Numbers = float(input("Write 5 numbers"))
+# Numbers = float(input("Write 5 numbers"))
+# Num = []
+
+# for i in range(Numbers):
+#     Number = input(f"Write your numbers {i+1}, ").strip()
+#     Num.append(Number)
+
+# largest = max(Numbers)
+
+# if Numbers < 5:
+#     print("Numbers are less than 5: TRY AGAIN")
+# print(f"Largest number given by you is {largest}")
+
+
 Num = []
 
-for i in range(Numbers):
-    Number = input(f"Write your numbers {i+1}, ").strip()
+for i in range(5):
+    Number = float(input(f"Five numbers {i+1}: "))
     Num.append(Number)
 
-largest = max(Numbers)
+largest = max(Num)
 
-if Numbers < 5:
-    print("Numbers are less than 5: TRY AGAIN")
-print(f"Largest number given by you is {largest}")
+print(f"Largest number given by you is {largest} ")
