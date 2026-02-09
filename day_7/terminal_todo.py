a4ff0767337c45b575a37830d49ede1c84a2a28c
Akshat Singh <nitfactor@gmail.com>
2026-02-09 15:10:02 +0530

    todo incomplete

--- day_7/terminal_todo.py
+++ day_7/terminal_todo.py
@@ -2,7 +2,7 @@ import os
 
 TASK_FILE = "tasks.txt"
 
-def load_task():
+def load_tasks():
     tasks = []
     if (os.path.exists(TASK_FILE)):
         with open(TASK_FILE, "r", encoding="UTF-8") as f:
@@ -11,4 +11,15 @@ def load_task():
                 tasks.append({"text": text, "done": status == "done"})
     return tasks
 
-    
+def task_manager():
+    tasks = load_tasks()
+
+    while True:
+        print("\n------Task List Manager -------")
+        print("1. Add task")
+        print("2. View Tasks")
+        print("3. Mark Task as complete")
+        print("4. Delete task")
+        print("5. Exit")
+
+        choice = input("Choose an option (1-5)").strip()
